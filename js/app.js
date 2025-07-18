ad64d3db9b4638f71e2ad59ae6caec9e56decf30
lucas-dias7 <w7.lucasdias@gmail.com>
2025-07-18 12:14:23 -0300

    excluir amigo incluido por clique no nome

--- js/app.js
+++ js/app.js
@@ -15,9 +15,12 @@ function adicionar () {
     } else {
         lista.textContent = lista.textContent + ', ' + amigo.value;
     }
-    amigo.value = '';
     } 
     }
+    amigo.value = '';
+
+    atualizarLista();
+    atualizarSorteio();
 }
 
 function sortear() {
@@ -37,6 +40,13 @@ function sortear() {
 
 }
 
+function excluirAmigo(index) {
+    amigos.splice(index, 1);
+    atualizarLista();
+    atualizarSorteio();
+}
+
+
 function embaralha(lista) {
 
     for (let indice = lista.length; indice; indice--) {
@@ -46,6 +56,27 @@ function embaralha(lista) {
         // atribuição via destructuring
         [lista[indice - 1], lista[indiceAleatorio]] = 
             [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function atualizarSorteio() {
+    let sorteio = document.getElementById('lista-sorteio');
+    sorteio.innerHTML = '';
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('lista-amigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        let paragrafo = document.createElement('p');
+        paragrafo.textContent = amigos[i];
+
+        paragrafo.addEventListener('click', function () {
+            excluirAmigo(i);
+        });
+
+        lista.appendChild(paragrafo);
     }
 }
 
